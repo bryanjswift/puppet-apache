bf54690a7a4d424a9780e8ddb8e339b69b9515a8
Bryan J Swift <bryan.j.swift@gmail.com>
2012-12-13 23:36:49 -0500

    Make it possible to pass owner and group to vhost
    owner and group are set on the directories for the vhost, parents and docroot

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -1,19 +1,31 @@
 define apache::vhost(
-    $port,
-    $parents='',
-    $docroot,
-    $logdir='/var/log/apache2',
-    $ssl=true,
-    $template='apache/vhost.conf.erb',
-    $priority,
-    $serveraliases='',
-    $options='Indexes FollowSymLinks MultiViews',
+  $port,
+  $parents       = '',
+  $docroot,
+  $logdir        = '/var/log/apache2',
+  $ssl           = true,
+  $template      = 'apache/vhost.conf.erb',
+  $priority,
+  $serveraliases = '',
+  $options       = 'Indexes FollowSymLinks MultiViews',
+  $owner         = false,
+  $group         = false,
 ) {
 
   include apache
 
+  if ($owner) {
+    File { owner => $owner }
+  }
+
+  if ($group) {
+    File { group => $group }
+  }
+
   file { "/etc/apache2/sites-enabled/${priority}-${name}":
     content => template($template),
+    owner   => 'root',
+    group   => 'root',
     mode    => 0744,
     require => [Class["apache::install"]],
     notify  => [Class["apache::service"]],
@@ -22,8 +34,6 @@ define apache::vhost(
   if ($parents != '') {
     file { $parents:
       ensure  => directory,
-      owner   => 'root',
-      group   => 'root',
       mode    => 0755,
       require => [],
     }
